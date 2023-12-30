31eb59fd204d08458a278ca36026f1ca53b93512
erik-meier <errmeier@gmail.com>
2023-12-30 15:10:37 -0600

    css setup

--- modules/sheets/item-sheet.js
+++ modules/sheets/item-sheet.js
@@ -1,4 +1,13 @@
 export default class McdmRpgItemSheet extends ItemSheet {
+    
+    static get defaultOptions () {
+        return mergeObject(super.defaultOptions, {
+            width: 530,
+            height: 340,
+            classes: ["mcdm-rpg", "sheet", "item"]
+        });
+    }
+    
     /* *
     * Get the html template based on the item type
     */
